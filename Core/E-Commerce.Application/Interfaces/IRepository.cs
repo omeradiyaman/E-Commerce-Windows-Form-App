c147364f102d3861d2fca64e6152c876769d3d00
Ömer A <omeradiyaman1169@gmail.com>
2024-10-16 13:50:58 +0300

    interfaces done.

---
+++ Core/E-Commerce.Application/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> GetById(int id);
+        Task<List<T>> GetAll();
+        Task<T> Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        int SaveChanges();
+    }
+}
